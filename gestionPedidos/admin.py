7b12b542746a9a4fab3eb15e4a47ba7d2d3a768b
lsomohano <lsomohano@avasa.com.mx>
2022-06-09 18:05:19 -0500

    Primer commit de formularios

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -11,6 +11,11 @@ class ArticulosAdmin(admin.ModelAdmin):
     list_display = ("nombre", "seccion", "precio")
     list_filter = ("seccion",)
 
+class PedidosAdmin(admin.ModelAdmin):
+    list_display = ("numero", "fecha", "entregado")
+    list_filter = ("fecha",)
+    date_hierarchy = "fecha"
+    
 admin.site.register(Clientes, ClientesAdmin)
 admin.site.register(Articulos, ArticulosAdmin)
-admin.site.register(Pedidos)
+admin.site.register(Pedidos, PedidosAdmin)
--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -1,3 +1,10 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 
 # Create your views here.
+def  busqueda_productos(request):
+    return render(request, "busqueda_productos.html")
+
+def buscar(request):
+    mensaje = "Articulo buscado: %r" %request.GET["producto"]
+    return HTTPResponse(mensaje)
