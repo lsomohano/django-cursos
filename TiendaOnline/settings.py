ead1061166ffa60e59666af7b2fb34c34415ced7
Leonel Somohano <lsomohano20@hotmail.com>
2023-01-23 13:04:54 -0500

    Update settings.py

--- TiendaOnline/settings.py
+++ TiendaOnline/settings.py
@@ -128,6 +128,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smpt.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER="lsomohano20@gmail.com"
+EMAIL_HOST_USER=""
 EMAIL_HOST_PASSWORD=""
 
